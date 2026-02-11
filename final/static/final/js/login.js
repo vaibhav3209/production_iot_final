486f06c1e0af7de0b274b2cec63461e9bdd30c0f
vaibhav3209 <vaibhavmalav35@gmail.com>
2026-02-11 14:40:58 +0530

    added show password

--- final/static/final/js/login.js
+++ final/static/final/js/login.js
@@ -7,5 +7,15 @@
     document.getElementById(formId).classList.remove('hidden');
   }
 
+function togglePassword() {
+    const passwordInput = document.getElementById("password");
+
+    if (passwordInput.type === "password") {
+        passwordInput.type = "text";
+    } else {
+        passwordInput.type = "password";
+    }
+}
+
   // Default form on page load
   toggleForm('student');
